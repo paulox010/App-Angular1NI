88c7334caf2cbf1f2829bd36545a54020265b9e3
paulox010 <115842836+paulox010@users.noreply.github.com>
2023-08-25 21:36:56 -0300

    Aula 25-08

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TituloComponent } from './components/titulo/titulo.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { BotaoComponent } from './components/botao/botao.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TituloComponent,
+    NavBarComponent,
+    BotaoComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/botao/botao.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-botao',
+  templateUrl: './botao.component.html',
+  styleUrls: ['./botao.component.css']
+})
+export class BotaoComponent {
+      @Input() conteudo:string='google.com'
+}
